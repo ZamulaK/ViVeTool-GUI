3e0c7fc0db99228531147a70e46ab7967a3f9d38
Zamula <zamulagh@gmail.com>
2022-09-30 11:40:29 -0400

    4.8.1

--- vivetool-gui/CrashReporter.vb
+++ vivetool-gui/CrashReporter.vb
@@ -39,22 +39,23 @@ Public Class CrashReporter
         SendReport(e.Exception)
     End Sub
 
-    ''' <summary>
-    ''' Sub that calls CrashReporter.Net to send an error report.
-    ''' </summary>
-    ''' <param name="exception">Exception to pass to CrashReporter.Net</param>
-    Private Shared Sub SendReport(exception As Exception)
-        Dim reportCrash = New ReportCrash(Nothing) With {
-            .AnalyzeWithDoctorDump = True,
-            .CaptureScreen = False,
-            .DoctorDumpSettings = New DoctorDumpSettings With {
-                .ApplicationID = New Guid("bc23cd2f-ae85-434d-83b0-d98254b1765f")
-            },
-            .EmailRequired = False,
-            .IncludeScreenshot = True,
-            .ShowScreenshotTab = True,
-            .DeveloperMessage = "Application Settings:" & Environment.NewLine & "AutoLoad: " & My.Settings.AutoLoad & Environment.NewLine & "Dark Mode: " & My.Settings.DarkMode & Environment.NewLine & "Use System Theme: " & My.Settings.UseSystemTheme
-        }
-        reportCrash.Send(exception)
-    End Sub
+  ''' <summary>
+  ''' Sub that calls CrashReporter.Net to send an error report.
+  ''' </summary>
+  ''' <param name="exception">Exception to pass to CrashReporter.Net</param>
+  Private Shared Sub SendReport(exception As Exception)
+    Exit Sub
+    Dim reportCrash = New ReportCrash(Nothing) With {
+        .AnalyzeWithDoctorDump = True,
+        .CaptureScreen = False,
+        .DoctorDumpSettings = New DoctorDumpSettings With {
+            .ApplicationID = New Guid("bc23cd2f-ae85-434d-83b0-d98254b1765f")
+        },
+        .EmailRequired = False,
+        .IncludeScreenshot = True,
+        .ShowScreenshotTab = True,
+        .DeveloperMessage = "Application Settings:" & Environment.NewLine & "AutoLoad: " & My.Settings.AutoLoad & Environment.NewLine & "Dark Mode: " & My.Settings.DarkMode & Environment.NewLine & "Use System Theme: " & My.Settings.UseSystemTheme
+    }
+    reportCrash.Send(exception)
+  End Sub
 End Class
